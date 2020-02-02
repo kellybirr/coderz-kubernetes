17151ca4f507c59e91895bff961c34dbd2232cec
Kelly Birr <kelly@kellycodes.com>
2020-02-02 12:58:06 -0700

    Initial Commit

---
+++ src/Coderz.Kubernetes.Extensions/ConfigExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Configuration
+{
+    public static class KubernetesConfigMapExtensions
+    {
+        public static IConfigurationBuilder AddConfigMapJsonFiles(this IConfigurationBuilder builder)
+        {
+            string fsRoot = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory());
+            string configRoot = Path.Combine(fsRoot, "configmaps");
+
+            return AddConfigMapJsonFiles(builder, configRoot);
+        }
+
+        public static IConfigurationBuilder AddConfigMapJsonFiles(this IConfigurationBuilder builder, string configRootPath)
+        {
+            if (Directory.Exists(configRootPath))
+            {
+                var configRootDir = new DirectoryInfo(configRootPath);
+                foreach (DirectoryInfo configDir in configRootDir.GetDirectories())
+                {
+                    foreach (FileInfo configFile in configDir.GetFiles("*.json"))
+                    {
+                        builder.AddJsonFile(configFile.FullName, optional: false, reloadOnChange: true);
+                    }
+                }
+            }
+
+            return builder;
+        }
+    }
+}
