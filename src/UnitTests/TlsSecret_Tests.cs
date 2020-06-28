a34ee2d459145a0a990c7a1341c4c7bbaea9911a
Kelly Birr <kelly@kellycodes.com>
2020-06-28 10:22:16 -0700

    testing in docker

--- src/UnitTests/TlsSecret_Tests.cs
+++ src/UnitTests/TlsSecret_Tests.cs
@@ -1,23 +1,40 @@
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using Coderz.Kubernetes.Extensions;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace UnitTests
 {
     public class TlsSecret_Tests
     {
+        private readonly string _basePath;
+        private readonly ITestOutputHelper _out;
+
+        public TlsSecret_Tests(ITestOutputHelper outputHelper)
+        {
+            _out = outputHelper;
+
+            _basePath = (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                ? "D:\\tls_kube_example"
+                : "/mnt/d/tls_kube_example";
+        }
+
         [Theory]
         [InlineData("rsa")]
         [InlineData("ecdsa")]
         public void LoadFromPath_Test(string alg)
         {
-            X509Certificate2Collection collection = TlsSecret.FromMappedPath("D:\\tls_kube_example\\"+alg);
+            string certPath = Path.Combine(_basePath, alg);
+            X509Certificate2Collection collection = TlsSecret.FromMappedPath(certPath);
             
             Assert.NotNull(collection);
             Assert.Equal(2, collection.Count);
             Assert.True(collection[0].HasPrivateKey);
+
+            _out.WriteLine(collection[0].ToString());
         }
 
         [Theory]
@@ -26,26 +43,29 @@ namespace UnitTests
         public void LoadFromStrings_Test(string alg)
         {
             // test setup
-            string basePath = "D:\\tls_kube_example\\" + alg;
-            string certString = File.ReadAllText(Path.Combine(basePath, "tls.crt"));
-            string keyString = File.ReadAllText(Path.Combine(basePath, "tls.key"));
+            string certPath = Path.Combine(_basePath, alg);
+            string certString = File.ReadAllText(Path.Combine(certPath, "tls.crt"));
+            string keyString = File.ReadAllText(Path.Combine(certPath, "tls.key"));
 
             // run test
             X509Certificate2Collection collection = TlsSecret.FromPemStrings(certString, keyString);
 
             Assert.NotNull(collection);
             Assert.Equal(2, collection.Count);
-            Assert.True(collection[0].HasPrivateKey);
+            Assert.True(collection[0].HasPrivateKey); 
+            
+            _out.WriteLine(collection[0].ToString());
         }
+
         [Theory]
         [InlineData("rsa")]
         [InlineData("ecdsa")]
         public void LoadFromEnvironment_Test(string alg)
         {
             // test setup
-            string basePath = "D:\\tls_kube_example\\" + alg;
-            string certString = File.ReadAllText(Path.Combine(basePath, "tls.crt"));
-            string keyString = File.ReadAllText(Path.Combine(basePath, "tls.key"));
+            string certPath = Path.Combine(_basePath, alg);
+            string certString = File.ReadAllText(Path.Combine(certPath, "tls.crt"));
+            string keyString = File.ReadAllText(Path.Combine(certPath, "tls.key"));
 
             Environment.SetEnvironmentVariable("tls_crt", certString);
             Environment.SetEnvironmentVariable("tls_key", keyString);
@@ -56,7 +76,8 @@ namespace UnitTests
             Assert.NotNull(collection);
             Assert.Equal(2, collection.Count);
             Assert.True(collection[0].HasPrivateKey);
-        }
 
+            _out.WriteLine(collection[0].ToString());
+        }
     }
 }
