bf501eaf694bbacc34deda9313aab53532bc523a
Kelly Birr <kelly@kellycodes.com>
2020-06-29 10:38:55 -0700

    tls-secrets (#1)
    * added support for TLS Certificates from Kubernetes TLS Secrets

---
+++ src/Coderz.Kubernetes.Extensions/X509CertificateExtensions.cs
@@ -0,0 +1,120 @@
+﻿using System.IO;
+using System.Linq;
+
+// ReSharper disable once CheckNamespace
+namespace System.Security.Cryptography.X509Certificates
+{
+    /// <summary>
+    /// Loads an X509Certificate2 from a Kubernetes TLS secret mapped as environment variables or as a file
+    /// </summary>
+    public static class X509CertificateExtensions
+    {
+        private const string OidRsa = "1.2.840.113549.1.1.1";
+        private const string OidEcc = "1.2.840.10045.2.1";
+
+        public static X509Certificate2 ImportFromEnvironment(this X509Certificate2Collection collection, string certVar="tls_crt", string keyVar="tls_key")
+        {
+            string tlsCertData = Environment.GetEnvironmentVariable(certVar);
+            string tlsKeyData = Environment.GetEnvironmentVariable(keyVar);
+
+            return ImportPemStrings(collection, tlsCertData, tlsKeyData);
+        }
+
+        public static X509Certificate2 ImportTlsSecret(this X509Certificate2Collection collection, string mappedPath)
+        {
+            if (!Directory.Exists(mappedPath))
+                return null;
+
+            var dir = new DirectoryInfo(mappedPath);
+            FileInfo tlsCertFile = dir.GetFiles("tls.crt").FirstOrDefault();
+            FileInfo tlsKeyFile = dir.GetFiles("tls.key").FirstOrDefault();
+
+            if (tlsCertFile == null) return null;
+            string tlsCertData = File.ReadAllText(tlsCertFile.FullName);
+
+            string tlsKeyData = (tlsKeyFile != null)
+                ? File.ReadAllText(tlsKeyFile.FullName)
+                : null;
+
+            return ImportPemStrings(collection, tlsCertData, tlsKeyData);
+        }
+
+        public static X509Certificate2 ImportPemStrings(this X509Certificate2Collection collection, string publicCertChain, string privateKey=null)
+        {
+            // get public cert chain
+            X509Certificate2 privateKeyCert = null;
+            string[] certChainParts = publicCertChain.Split('-', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < certChainParts.Length; i++)
+            {
+                if ((certChainParts[i] == "BEGIN CERTIFICATE") && (i < (certChainParts.Length - 1)))
+                {
+                    // decode certificate from base64 in next string
+                    byte[] certBuffer = Convert.FromBase64String(certChainParts[++i]);
+                    var cert = new X509Certificate2(certBuffer);
+
+                    // check if we need to add private key, if the first cert in the PEM
+                    if (privateKeyCert == null && !string.IsNullOrWhiteSpace(privateKey))
+                    {
+                        cert = cert.PublicKey.Oid.Value switch
+                        {
+                            OidRsa => SetRsaPrivateKey(cert, privateKey),
+                            OidEcc => SetEcDsaPrivateKey(cert, privateKey),
+                            _ => throw new ArgumentException("Invalid Certificate Type for Private Key", nameof(privateKey))
+                        };
+
+                        privateKeyCert = cert;
+                    }
+
+                    collection.Add(cert);
+                }
+            }
+
+            return privateKeyCert;
+        }
+
+        private static X509Certificate2 SetRsaPrivateKey(X509Certificate2 cert, string privateKey)
+        {
+            string[] keyParts = privateKey.Split('-', StringSplitOptions.RemoveEmptyEntries);
+            byte[] keyBytes = Convert.FromBase64String(keyParts[1]);
+
+            RSA rsaPrivateKey = RSA.Create();
+            switch (keyParts[0])
+            {
+                case "BEGIN PRIVATE KEY":
+                    rsaPrivateKey.ImportPkcs8PrivateKey(keyBytes, out _);
+                    break;
+                case "BEGIN RSA PRIVATE KEY":
+                    rsaPrivateKey.ImportRSAPrivateKey(keyBytes, out _);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid PrivateKey String", nameof(privateKey));
+            }
+
+            return cert.CopyWithPrivateKey(rsaPrivateKey);
+        }
+
+        private static X509Certificate2 SetEcDsaPrivateKey(X509Certificate2 cert, string privateKey)
+        {
+            string[] keyParts = privateKey.Split('-', StringSplitOptions.RemoveEmptyEntries);
+            byte[] keyBytes = Convert.FromBase64String(keyParts[1]);
+
+            ECDsa eccPrivateKey = ECDsa.Create();
+            if (eccPrivateKey == null) 
+                throw new PlatformNotSupportedException("Unable to create ECDsa");
+
+            switch (keyParts[0])
+            {
+                case "BEGIN PRIVATE KEY":
+                    eccPrivateKey.ImportPkcs8PrivateKey(keyBytes, out _);
+                    break;
+                case "BEGIN EC PRIVATE KEY":
+                    eccPrivateKey.ImportECPrivateKey(keyBytes, out _);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid PrivateKey String", nameof(privateKey));
+            }
+
+            return cert.CopyWithPrivateKey(eccPrivateKey);
+        }
+    }
+}
---
+++ src/UnitTests/TlsSecret_Tests.cs
@@ -0,0 +1,122 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public class TlsSecret_Tests
+    {
+        private readonly string _basePath;
+        private readonly ITestOutputHelper _out;
+
+        public TlsSecret_Tests(ITestOutputHelper outputHelper)
+        {
+            _out = outputHelper;
+
+            _basePath = (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                ? "D:\\tls_kube_example"
+                : "/mnt/d/tls_kube_example";
+        }
+
+        [Theory]
+        [InlineData("rsa")]
+        [InlineData("ecdsa")]
+        public void LoadFromPath_Test(string alg)
+        {
+            // test setup
+            string certPath = Path.Combine(_basePath, alg);
+
+            // run test
+            var certificates = new X509Certificate2Collection();
+            X509Certificate2 privateKeyCert = certificates.ImportTlsSecret(certPath);
+
+            Assert.Equal(2, certificates.Count);
+
+            Assert.NotNull(privateKeyCert);
+            Assert.True(privateKeyCert.HasPrivateKey);
+
+            Assert.Equal(privateKeyCert, certificates[0]);
+            Assert.False(certificates[1].HasPrivateKey);
+
+            _out.WriteLine(privateKeyCert.ToString());
+        }
+
+        [Theory]
+        [InlineData("rsa")]
+        [InlineData("ecdsa")]
+        public void LoadFromStrings_Test(string alg)
+        {
+            // test setup
+            string certPath = Path.Combine(_basePath, alg);
+            string certString = File.ReadAllText(Path.Combine(certPath, "tls.crt"));
+            string keyString = File.ReadAllText(Path.Combine(certPath, "tls.key"));
+
+            // run test
+            var certificates = new X509Certificate2Collection();
+            X509Certificate2 privateKeyCert = certificates.ImportPemStrings(certString, keyString);
+
+            Assert.Equal(2, certificates.Count);
+
+            Assert.NotNull(privateKeyCert);
+            Assert.True(privateKeyCert.HasPrivateKey); 
+
+            Assert.Equal(privateKeyCert, certificates[0]);
+            Assert.False(certificates[1].HasPrivateKey);
+
+            _out.WriteLine(privateKeyCert.ToString());
+        }
+
+        [Theory]
+        [InlineData("rsa")]
+        [InlineData("ecdsa")]
+        public void LoadFromEnvironment_Test(string alg)
+        {
+            // test setup
+            string certPath = Path.Combine(_basePath, alg);
+            string certString = File.ReadAllText(Path.Combine(certPath, "tls.crt"));
+            string keyString = File.ReadAllText(Path.Combine(certPath, "tls.key"));
+
+            Environment.SetEnvironmentVariable("tls_crt", certString);
+            Environment.SetEnvironmentVariable("tls_key", keyString);
+
+            // run test
+            var certificates = new X509Certificate2Collection();
+            X509Certificate2 privateKeyCert = certificates.ImportFromEnvironment();
+
+            Assert.Equal(2, certificates.Count);
+
+            Assert.NotNull(privateKeyCert);
+            Assert.True(privateKeyCert.HasPrivateKey); 
+
+            Assert.Equal(privateKeyCert, certificates[0]);
+            Assert.False(certificates[1].HasPrivateKey);
+
+            _out.WriteLine(certificates[0].ToString());
+        }
+
+        [Theory]
+        [InlineData("rsa")]
+        [InlineData("ecdsa")]
+        public void NoPrivateKey_Test(string alg)
+        {
+            // test setup
+            string certPath = Path.Combine(_basePath, alg);
+            string certString = File.ReadAllText(Path.Combine(certPath, "tls.crt"));
+
+            // run test
+            var certificates = new X509Certificate2Collection();
+            X509Certificate2 privateKeyCert = certificates.ImportPemStrings(certString);
+
+            Assert.Equal(2, certificates.Count);
+
+            Assert.Null(privateKeyCert);
+
+            Assert.False(certificates[0].HasPrivateKey);
+            Assert.False(certificates[1].HasPrivateKey);
+        }
+
+    }
+}
